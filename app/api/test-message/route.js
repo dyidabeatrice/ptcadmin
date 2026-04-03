36f0d2971b702ac87bfc4b8401de4a382280be5a
Ida Beatrice Dy <idabeatricedy@macbookair.home>
2026-04-03 18:27:09 +0200

    changed test-message

--- app/api/test-message/route.js
+++ app/api/test-message/route.js
@@ -2,18 +2,9 @@ export async function GET() {
   const PAGE_TOKEN = process.env.META_PAGE_ACCESS_TOKEN
   const PAGE_ID = process.env.META_PAGE_ID
 
-  const res = await fetch(
-    `https://graph.facebook.com/v19.0/${PAGE_ID}/messages?access_token=${PAGE_TOKEN}`,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        recipient: { id: '4801456173302861' },
-        message: { text: 'Test reminder from PTCAdmin!' }
-      })
-    }
-  )
-
-  const data = await res.json()
-  return Response.json(data)
+  return Response.json({
+    page_id: PAGE_ID,
+    token_start: PAGE_TOKEN?.substring(0, 20),
+    token_length: PAGE_TOKEN?.length
+  })
 }
